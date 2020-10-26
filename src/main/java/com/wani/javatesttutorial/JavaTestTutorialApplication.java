8a8c83cb7559b6462a792f27ee5d181774020903
oen142 <oen142@naver.com>
2020-10-26 23:03:24 +0900

    git init

---
+++ src/main/java/com/wani/javatesttutorial/JavaTestTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.wani.javatesttutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTestTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wani/javatesttutorial/JavaTestTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wani.javatesttutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
